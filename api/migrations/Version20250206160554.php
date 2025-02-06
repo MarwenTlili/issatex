187525e8f6bc6210bfad7a8c75bdebde6541b43f
MarwenTlili <marwentlili@gmail.com>
2025-02-06 17:09:42 +0100

    migration

--- api/migrations/Version20250206160554.php
+++ api/migrations/Version20250206160554.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250202222941 extends AbstractMigration
+final class Version20250206160554 extends AbstractMigration
 {
     public function getDescription(): string
     {
