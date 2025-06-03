a58d93be6e60b003144b56a01674762d3da5454a
MarwenTlili <marwentlili@gmail.com>
2025-06-03 16:13:43 +0100

    fix: session serialization (missing id, roles, ...)

--- pwa/src/components/header.tsx
+++ pwa/src/components/header.tsx
@@ -1,8 +1,9 @@
 import { getServerSession } from "next-auth";
 import HeaderComponent from "./header-component";
+import { authOptions } from "@/lib/auth/auth-options";
 
 export default async function Header() {
-  const session = await getServerSession();
+  const session = await getServerSession(authOptions);
 
   return (
     <HeaderComponent
