450381bfb245e0491342766e4c5f229015708a81
MarwenTlili <marwentlili@gmail.com>
2025-10-16 11:48:46 +0100

    opt: unused helper

--- api/src/DataFixtures/Helper.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\DataFixtures;
-
-class Helper {
-  public static function roundUpToNearest(int $number, int $nearest): int {
-    return intval(ceil($number / $nearest) * $nearest);
-  }
-}
