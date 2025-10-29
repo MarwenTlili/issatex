66bcbb2fd7178d00b8457ffc2581fbc6ad6371e1
MarwenTlili <marwentlili@gmail.com>
2025-10-29 15:58:48 +0100

    fix: workshop CRUD

--- pwa/src/components/admin/ilots/IlotList.tsx
+++ pwa/src/components/admin/ilots/IlotList.tsx
@@ -4,31 +4,62 @@ import {
   TextField,
   SimpleList,
   SearchInput,
-  EditButton,
+  useRecordContext,
 } from "react-admin";
 import { useMediaQuery, Theme } from "@mui/material";
+import { Ilot } from "@/types/resources/Ilot";
+import RowActions from "@/components/admin/common/row-actions";
 
-const filters = [<SearchInput key="search" source="ref" alwaysOn />];
+const ilotListFilters = [<SearchInput key="search" source="ref" alwaysOn />];
+
+const RefField = () => {
+  const record = useRecordContext<Ilot>();
+  if (!record) return null;
+
+  return (
+    <TextField
+      source="ref"
+      title={record.ref}
+      sx={{
+        maxWidth: 150,
+        whiteSpace: "nowrap",
+        overflow: "hidden",
+        textOverflow: "ellipsis",
+        display: "block",
+      }}
+    />
+  );
+};
 
 export const IlotList = () => {
   const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.down("sm"));
 
   return (
-    <List filters={filters}>
+    <List filters={ilotListFilters}>
       {isSmall ? (
-        // Vue mobile
+        // Mobile display
         <SimpleList
           primaryText={(record) => record.nom}
-          secondaryText={(record) => record.ref}
-          tertiaryText={(record) => record.description}
+          secondaryText={(record) => record.description}
+          tertiaryText={(record) => record.ref}
         />
       ) : (
-        // Vue desktop
-        <Datagrid rowClick="show">
-          <TextField source="ref" />
+        // Desktop display
+        <Datagrid rowClick={false}>
+          <RefField />
           <TextField source="nom" />
-          <TextField source="description" />
-          <EditButton />
+          <TextField
+            source="description"
+            sx={{
+              display: "-webkit-box",
+              WebkitLineClamp: 1,
+              WebkitBoxOrient: "vertical",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              maxWidth: 250,
+            }}
+          />
+          <RowActions<Ilot> resource="api/ilots" />
         </Datagrid>
       )}
     </List>
