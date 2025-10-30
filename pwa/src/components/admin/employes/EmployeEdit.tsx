83b6a59164a579b36b977eb46de5d72491af8899
MarwenTlili <marwentlili@gmail.com>
2025-10-30 15:35:08 +0100

    fix: reuse row actions and refactor

--- pwa/src/components/admin/employes/EmployeEdit.tsx
+++ pwa/src/components/admin/employes/EmployeEdit.tsx
@@ -1,18 +1,17 @@
+import { Employe } from "@/types/resources/Employe";
 import React from "react";
-import { Edit, SimpleForm, TextInput } from "react-admin";
+import { Edit, SimpleForm, TextInput, useRecordContext } from "react-admin";
+
+const CustomTitle = () => {
+  const record = useRecordContext<Employe>();
+  return `${record?.ref}`;
+};
 
 export const EmployeEdit = () => {
   return (
-    <Edit>
-      <SimpleForm
-        sx={{
-          "& .RaSimpleForm-form": {
-            backgroundColor: "var(--color-card)",
-            padding: "24px",
-            borderRadius: "8px",
-          },
-        }}
-      >
+    <Edit title={<CustomTitle />} redirect="list">
+      <SimpleForm>
+        <TextInput source="ref" disabled />
         <TextInput source="nom" label="Last Name" required />
         <TextInput source="prenom" label="First Name" required />
         <TextInput source="poste" label="Position" required />
