b6a4b3c2798b0320c89af698c9b4fe56fab33e64
MarwenTlili <marwentlili@gmail.com>
2025-11-01 14:27:58 +0100

    fix: profile's logout position

--- pwa/src/components/admin/layout/CustomAppBar.tsx
+++ pwa/src/components/admin/layout/CustomAppBar.tsx
@@ -1,55 +1,40 @@
 import React from "react";
-import { AppBar, TitlePortal } from "react-admin";
-import { Box, Typography, useTheme } from "@mui/material";
+import { AppBar, AppBarProps, TitlePortal, useAuthProvider } from "react-admin";
+import { Box, Theme, Typography, useMediaQuery, useTheme } from "@mui/material";
 import FactoryIcon from "@mui/icons-material/Factory";
 
-const CustomAppBar = () => {
+const CustomAppBar = ({ classes, userMenu, ...props }: AppBarProps) => {
+  const authProvider = useAuthProvider();
   const theme = useTheme();
+  const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.up("sm"));
 
   const isDark = theme.palette.mode === "dark";
-
   const logoColor = isDark ? "#ffffff" : "AliceBlue";
 
   return (
-    <AppBar>
+    <AppBar userMenu={userMenu ?? !!authProvider} {...props}>
+      {isSmall && <TitlePortal />}
+      {!isSmall && <Box component="span" sx={{ flex: 1 }} />}
+
       <Box
         sx={{
-          position: "relative",
-          width: "100%",
           display: "flex",
           alignItems: "center",
+          gap: 1,
+          flexGrow: 1,
         }}
       >
-        {/* LEFT: menu button + TitlePortal */}
-        <Box sx={{ display: { xs: "none", sm: "flex" }, alignItems: "center" }}>
-          <TitlePortal />
-        </Box>
-
-        {/* CENTER: logo + "Admin" */}
-        <Box
-          sx={{
-            position: "absolute",
-            left: "50%",
-            transform: "translateX(-50%)",
-            display: "flex",
-            alignItems: "center",
-            gap: 1,
-            pointerEvents: "none", // clicks go through to RA actions
+        <FactoryIcon fontSize="large" style={{ color: logoColor }} />
+        <Typography
+          variant="h6"
+          style={{
+            fontFamily: "Roboto, sans-serif",
+            fontWeight: 700,
+            fontSize: 20,
           }}
         >
-          <FactoryIcon fontSize="large" style={{ color: logoColor }} />
-
-          <Typography
-            variant="h6"
-            style={{
-              fontFamily: "Roboto, sans-serif",
-              fontWeight: 700,
-              fontSize: 20,
-            }}
-          >
-            Issatex
-          </Typography>
-        </Box>
+          Issatex
+        </Typography>
       </Box>
     </AppBar>
   );
