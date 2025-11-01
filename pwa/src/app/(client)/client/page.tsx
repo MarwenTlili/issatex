8d9c3449a0d9b33f310630113badb32bd221c767
MarwenTlili <marwentlili@gmail.com>
2025-11-01 20:11:33 +0100

    refactor

--- pwa/src/app/(client)/client/page.tsx
+++ pwa/src/app/(client)/client/page.tsx
@@ -1,9 +1,5 @@
 import ClientDashboard from "@/components/dashboards/client-dashboard";
 
 export default async function ClientPage() {
-  return (
-    <>
-      <ClientDashboard />
-    </>
-  );
+  return <ClientDashboard />;
 }
--- pwa/src/app/(secretaire)/secretaire/page.tsx
+++ pwa/src/app/(secretaire)/secretaire/page.tsx
@@ -1,8 +1,5 @@
 import { SecretaryDashboard } from "@/components/dashboards/secretaire-dashboard";
-import React from "react";
 
 export default async function SecretaryHomePage() {
-  return <div className="m-2 md:m-4">
-    <SecretaryDashboard />
-  </div>;
+  return <SecretaryDashboard />;
 }
