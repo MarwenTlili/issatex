2408d7a21825b86f2abc82fe654176c4d1f01550
MarwenTlili <marwentlili@gmail.com>
2025-11-05 21:05:32 +0100

    opt: responsive

--- pwa/src/components/admin/articles/ArticleList.tsx
+++ pwa/src/components/admin/articles/ArticleList.tsx
@@ -1,12 +1,27 @@
+import { Article } from "@/types/resources/Article";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardHeader,
+  Stack,
+  Theme,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
 import {
   List,
   Datagrid,
   TextField,
-  ReferenceField,
   ReferenceInput,
   AutocompleteInput,
   SearchInput,
+  Identifier,
+  useListContext,
 } from "react-admin";
+import RowActions from "@/components/admin/common/row-actions";
+import { ClientReferenceField } from "@/components/admin/common/fields/ClientReferenceField";
+import { OrdreFabricationsReferenceArrayField } from "@/components/admin/common/fields/OrdreFabricationsReferenceArrayField";
 
 const filters = [
   <SearchInput key="search" source="ref" alwaysOn />,
@@ -15,24 +30,79 @@ const filters = [
   </ReferenceInput>,
 ];
 
-export const ArticleList = () => (
-  <List filters={filters}>
-    <Datagrid rowClick="show">
-      <TextField source="ref" />
-      <TextField source="designation" />
-      <ReferenceField source="client" reference="api/clients" link="show">
-        <TextField source="nom" />
-      </ReferenceField>
-      <ReferenceField
-        label="email"
-        source="client"
-        reference="api/clients"
-        link={false}
-      >
-        <ReferenceField source="account" reference="api/users" link={false}>
-          <TextField source="email" />
-        </ReferenceField>
-      </ReferenceField>
-    </Datagrid>
-  </List>
-);
+const MobileArticleList = () => {
+  const { data, isLoading } = useListContext<Article & { id: Identifier }>();
+
+  if (isLoading) {
+    return <Box sx={{ p: 2 }}>Loading...</Box>;
+  }
+
+  return (
+    <Stack spacing={3} sx={{ p: 2 }}>
+      {data?.map((record) => (
+        <Card key={record.id} className="border-l-4 border-l-primary">
+          <CardHeader
+            action={
+              <Stack alignItems="flex-end" spacing={1}>
+                <RowActions<Article> resource="api/articles" record={record} />
+              </Stack>
+            }
+            title={record.designation}
+            subheader={record.ref}
+          />
+          <CardContent>
+            <Stack spacing={2}>
+              <div>
+                <p className="text-muted-foreground font-medium">COMPOSITION</p>
+                <Typography
+                  variant="body2"
+                  sx={{
+                    display: "-webkit-box",
+                    WebkitLineClamp: 2,
+                    WebkitBoxOrient: "vertical",
+                    overflow: "hidden",
+                  }}
+                >
+                  {record.composition}
+                </Typography>
+              </div>
+              <div>
+                <p className="text-muted-foreground font-medium">CLIENT</p>
+                <ClientReferenceField record={record} />
+              </div>
+              <div>
+                <p className="text-muted-foreground font-medium">
+                  ORDRES DE FABRICATION
+                </p>
+                <OrdreFabricationsReferenceArrayField record={record} />
+              </div>
+            </Stack>
+          </CardContent>
+        </Card>
+      ))}
+    </Stack>
+  );
+};
+
+export const ArticleList = () => {
+  const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.down("sm"));
+
+  return (
+    <List filters={filters}>
+      {isSmall ? (
+        <MobileArticleList />
+      ) : (
+        <Datagrid rowClick={false}>
+          <TextField source="ref" />
+          <TextField source="designation" />
+          <ClientReferenceField label="Client" />
+          <OrdreFabricationsReferenceArrayField label="Ordres de Fabrication" />
+          <RowActions
+            resource="api/articles"
+            hideActions={{ edit: true, delete: true }}
+          />
+        </Datagrid>
+      )}
+    </List>
+  );
+};
