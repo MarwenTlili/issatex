545da019df8a5454832be2858efcfc2da9876d61
MarwenTlili <marwentlili@gmail.com>
2025-11-05 21:05:32 +0100

    opt: responsive

--- pwa/src/components/admin/articles/ArticleShow.tsx
+++ pwa/src/components/admin/articles/ArticleShow.tsx
@@ -1,12 +1,6 @@
-import {
-  Show,
-  SimpleShowLayout,
-  TextField,
-  ReferenceField,
-  ReferenceManyField,
-  Datagrid,
-  DateField,
-} from "react-admin";
+import { Show, SimpleShowLayout, TextField } from "react-admin";
+import { ClientReferenceField } from "@/components/admin/common/fields/ClientReferenceField";
+import { OrdreFabricationsReferenceArrayField } from "@/components/admin/common/fields/OrdreFabricationsReferenceArrayField";
 
 export const ArticleShow = () => (
   <Show>
@@ -14,32 +8,8 @@ export const ArticleShow = () => (
       <TextField source="ref" />
       <TextField source="designation" />
       <TextField source="composition" />
-      <ReferenceField source="client" reference="api/clients" link="show">
-        <TextField source="nom" />
-      </ReferenceField>
-      <ReferenceField
-        label="email"
-        source="client"
-        reference="api/clients"
-        link={false}
-      >
-        <ReferenceField source="account" reference="api/users" link={false}>
-          <TextField source="email" />
-        </ReferenceField>
-      </ReferenceField>
-      <ReferenceManyField
-        label="Ordres de Fabrication"
-        reference="api/ordre_fabrications"
-        target="article"
-      >
-        <Datagrid rowClick="show">
-          <TextField source="ref" />
-          <DateField source="dateCreation" />
-          <DateField source="dateCloture" />
-          <TextField source="statut" />
-          <TextField source="quantiteTotale" />
-        </Datagrid>
-      </ReferenceManyField>
+      <ClientReferenceField label="Client" />
+      <OrdreFabricationsReferenceArrayField label="Ordres de Fabrication" />
     </SimpleShowLayout>
   </Show>
 );
