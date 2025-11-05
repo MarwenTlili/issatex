47aecaa76fabda21e61878e1b4051c0e0697723b
MarwenTlili <marwentlili@gmail.com>
2025-11-05 21:10:28 +0100

    opt: custom title

--- pwa/src/components/admin/articles/ArticleShow.tsx
+++ pwa/src/components/admin/articles/ArticleShow.tsx
@@ -1,9 +1,20 @@
-import { Show, SimpleShowLayout, TextField } from "react-admin";
+import {
+  Show,
+  SimpleShowLayout,
+  TextField,
+  useRecordContext,
+} from "react-admin";
 import { ClientReferenceField } from "@/components/admin/common/fields/ClientReferenceField";
 import { OrdreFabricationsReferenceArrayField } from "@/components/admin/common/fields/OrdreFabricationsReferenceArrayField";
+import { Article } from "@/types/resources/Article";
+
+const CustomTitle = () => {
+  const record = useRecordContext<Article>();
+  return `${record?.ref}`;
+};
 
 export const ArticleShow = () => (
-  <Show>
+  <Show title={<CustomTitle />}>
     <SimpleShowLayout>
       <TextField source="ref" />
       <TextField source="designation" />
