b58998d8819a8cfdb9a95ae93b43d5aa04b924c9
MarwenTlili <marwentlili@gmail.com>
2026-01-01 19:44:04 +0100

    opt: optimize JWT authentication

--- api/src/EventListener/JWTCreatedListener.php
+++ api/src/EventListener/JWTCreatedListener.php
@@ -2,21 +2,10 @@
 
 namespace App\EventListener;
 
-use App\Entity\User;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
-use Symfony\Component\HttpFoundation\RequestStack;
 
 class JWTCreatedListener {
-    /**
-     * @var RequestStack
-     */
-    private $requestStack;
-
-    /**
-     * @param RequestStack $requestStack
-     */
-    public function __construct(RequestStack $requestStack) {
-        $this->requestStack = $requestStack;
+    public function __construct() {
     }
 
     /**
@@ -25,64 +14,30 @@ class JWTCreatedListener {
      * @return void
      */
     public function onJWTCreated(JWTCreatedEvent $event) {
-        /**
-         * $event->getData(): ["username" => "...", "roles": [...]]
-         */
-        /** @var User $user */
+        /** @var \App\Entity\User $user */
         $user = $event->getUser();
 
-        /** @var Avatar */
+        /** @var \App\Entity\Avatar */
         $avatar = $user->getAvatar();
 
-        /**
-         * [string[] roles, string username]
-         * $payload = $event->getData();
-         */
-        $payload = [];
+        $payload = $event->getData();
 
+        // === Standard / enterprise claims ===
         $payload['iss'] = 'https://localhost';
-
-        /** add user's ID to toen payload */
+        $payload['aud'] = 'client-id';
         $payload['sub'] = $user->getId();
 
-        $payload['aud'] = 'client-id';
-
-        /** Override token expiration date calculation to be more flexible (defautlt +1 hour) */
-        // $expiration = new \DateTime('+1 day');
-        // $expiration->setTime(2, 0, 0);
-        // $payload['exp'] = $expiration->getTimestamp();
-
-        /** Check lexik_jwt_authentication: token_ttl */
-        $expiration = new \DateTime('+1 hour');
-        $payload['exp'] = $expiration->getTimestamp(); // Unix TimeStamp (sec)
-        // $payload['exp (date)'] = (new \DateTime())->setTimestamp($expiration->getTimestamp())->format('Y-m-d H:i:s');
-
-        /** Token valid only after 60 seconds from now */
-        // $payload['nbf'] = time() + 60;
-
-        $now = new \DateTime();
-        $payload['iat'] = $now->getTimestamp(); // Unix TimeStamp (sec)
-        // $payload['iat (date)'] = (new \DateTime())->setTimestamp($now->getTimestamp())->format('Y-m-d H:i:s');
-
-        /** Add a unique identifier */
-        $payload['jti'] = uniqid();
-
-        /** add user's username to token payload */
+        // === Business claims ===
         $payload['username'] = $user->getUsername();
-
         $payload['email'] = $user->getEmail();
         $payload['roles'] = $user->getRoles();
         $payload['avatar'] = $avatar ? '/uploads/avatars/' . $avatar->getFilePath() : '';
 
-        /** add client ip address to payload */
-        // $request = $this->requestStack->getCurrentRequest();
-        // $payload['ip'] = $request->getClientIp();
-
         $event->setData($payload);
 
+        // Optional but recommended
         $header = $event->getHeader();
         $header['cty'] = 'JWT';
-
         $event->setHeader($header);
     }
 }
