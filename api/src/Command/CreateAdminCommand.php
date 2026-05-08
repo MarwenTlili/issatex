9958eb1c00ea7fd2605d752702c6869b5ac34438
MarwenTlili <marwentlili@gmail.com>
2026-05-08 12:48:05 +0100

    fix: create-admin user command and make it munual

--- api/src/Command/CreateAdminCommand.php
+++ api/src/Command/CreateAdminCommand.php
@@ -14,30 +14,38 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 /**
  * $ php bin/console app:create-admin
  */
-#[AsCommand(name: 'app:create-admin')]
+#[AsCommand(
+  name: 'app:create-admin',
+  description: "Create admin user using environment variables"
+)]
 class CreateAdminCommand extends Command {
   public function __construct(
     private UserRepository $repo,
     private UserPasswordHasherInterface $hasher,
     private EntityManagerInterface $em,
+    private ?string $adminEmail,
+    private ?string $adminUsername,
+    private ?string $adminPassword
   ) {
     parent::__construct();
   }
 
   protected function execute(InputInterface $input, OutputInterface $output): int {
-    $email = getenv('ADMIN_EMAIL');
-    $username = getenv('ADMIN_USERNAME');
+    if (!$this->adminEmail || !$this->adminUsername || !$this->adminPassword) {
+      $output->writeln('<error>Missing ADMIN_* environment variables.</error>');
+      return Command::FAILURE;
+    }
 
-    if ($this->repo->findOneBy(['email' => $email])) {
+    if ($this->repo->findOneBy(['email' => $this->adminEmail])) {
       $output->writeln('Admin already exists.');
       return Command::SUCCESS;
     }
 
     $user = new User();
-    $user->setEmail($email)
-      ->setUsername($username)
+    $user->setEmail($this->adminEmail)
+      ->setUsername($this->adminUsername)
       ->setPassword(
-        $this->hasher->hashPassword($user, getenv('ADMIN_PASSWORD'))
+        $this->hasher->hashPassword($user, $this->adminPassword)
       )
       ->setRoles(['ROLE_ADMIN'])
       ->setEnabled(true);
@@ -45,7 +53,7 @@ class CreateAdminCommand extends Command {
     $this->em->persist($user);
     $this->em->flush();
 
-    $output->writeln('Admin created.');
+    $output->writeln('Default admin user created.');
 
     return Command::SUCCESS;
   }
